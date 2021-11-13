ba390022ed5b210b2f47c7ce2e25aec6c053b608
Alexander Jacoby <alex_jacoby@brown.edu>
2021-11-13 01:22:31 -0500

    Added all relevant operators for Hamiltonian of Bilinear-Biquadratic.

--- OPS.jl
+++ OPS.jl
@@ -48,3 +48,75 @@ function Bilin(n::Int64,m::Int64,L::Int64)
         return kron(opsarray1...)+kron(opsarray2...)+kron(opsarray3...)
     end
 end
+
+
+
+function Biquad(n::Int64,m::Int64,L::Int64)
+    opsarray11 = []
+    opsarray21 = []
+    opsarray31 = []
+    opsarray12 = []
+    opsarray22 = []
+    opsarray32 = []
+    opsarray13 = []
+    opsarray23 = []
+    opsarray33 = []
+    if m == n
+        for i in 1:L
+            push!(opsarray11,id)
+            push!(opsarray21,id)
+            push!(opsarray31,id)
+            push!(opsarray12,id)
+            push!(opsarray22,id)
+            push!(opsarray32,id)
+            push!(opsarray13,id)
+            push!(opsarray23,id)
+            push!(opsarray33,id)
+        end
+        opsarray11[n] = (sz^2)*(sz^2)
+        opsarray21[n] = (sy^2)*(sz^2)
+        opsarray31[n] = (sx^2)*(sz^2)
+        opsarray12[n] = (sz^2)*(sy^2)
+        opsarray22[n] = (sy^2)*(sy^2)
+        opsarray32[n] = (sx^2)*(sy^2)
+        opsarray13[n] = (sz^2)*(sx^2)
+        opsarray23[n] = (sy^2)*(sx^2)
+        opsarray33[n] = (sx^2)*(sx^2)
+        return kron(opsarray11...)+kron(opsarray21...)+kron(opsarray31...)+kron(opsarray12...)+kron(opsarray22...)+kron(opsarray32...)+kron(opsarray13...)+kron(opsarray23...)+kron(opsarray33...)
+    else
+        for i in 1:L
+            if (i != n) & (i != m)
+                push!(opsarray11,id)
+                push!(opsarray21,id)
+                push!(opsarray31,id)
+                push!(opsarray12,id)
+                push!(opsarray22,id)
+                push!(opsarray32,id)
+                push!(opsarray13,id)
+                push!(opsarray23,id)
+                push!(opsarray33,id)
+            elseif i == n # these entries are wrong-- these should be bilinear in these operators
+                push!(opsarray11,sz^2)
+                push!(opsarray21,sz*sy)
+                push!(opsarray31,sz*sx)
+                push!(opsarray12,sy*sz)
+                push!(opsarray22,sy^2)
+                push!(opsarray32,sy*sx)
+                push!(opsarray13,sx*sz)
+                push!(opsarray23,sx*sy)
+                push!(opsarray33,sx^2)
+            elseif i == m
+                push!(opsarray11,sz^2)
+                push!(opsarray21,sz*sy)
+                push!(opsarray31,sz*sx)
+                push!(opsarray12,sy*sz)
+                push!(opsarray22,sy^2)
+                push!(opsarray32,sy*sx)
+                push!(opsarray13,sx*sz)
+                push!(opsarray23,sx*sy)
+                push!(opsarray33,sx^2)
+            end
+        end
+        return kron(opsarray11...)+kron(opsarray21...)+kron(opsarray31...)+kron(opsarray12...)+kron(opsarray22...)+kron(opsarray32...)+kron(opsarray13...)+kron(opsarray23...)+kron(opsarray33...)
+    end
+end
--- TestFile.jl
+++ TestFile.jl
@@ -3,21 +3,28 @@ include("Permutation_Matrices.jl")
 include("InfoTools.jl")
 include("Ops.jl")
 L= 9#N^2
-samples = 100
+samples = 200
 
 psi = zeros(Complex{Float64},3^L)
 paths = walks(L,samples)
 pathmats = WalkMats(paths)
 
 
+avglength = 0
 for i in 1:samples
-    dist = sum(Distances(paths[i],3))
-    psi = psi + exp(0.2*dist+5)*BuildVBS(pathmats[i,:,:])
+    avglength = avglength+sum(Distances(paths[i],3))*(1/samples)
+end
+avglength/9
+α = 0.5
+β = 1.5
+for i in 1:samples
+    dist = sum(Distances(paths[i],3))/L
+    psi = psi + exp((avglength-β)^2/α)*exp(-(dist-β)^2/α)*BuildVBS(pathmats[i,:,:])
     print("x")
 end
 normalize!(psi)
 
-(norm(Collapse(3,9,9,psi)))^2
+(norm(Collapse(1,9,9,psi)))^2
 
 
 
@@ -25,14 +32,13 @@ normalize!(psi)
 DM = Sparse_DM_Build(sparse(psi))
 Partial_Trace(DM, [1,2,3,4,6,7,8,9],9)
 for i in 1:9
-    print(dot(psi,Bilin(1,i,9) *psi))
+    print(dot(psi,Bilin(5,i,9) *psi),"   ")
 end
-dot(psi,Bilin(1,7,9) *psi)
-Bilin(1,2,16)
+dot(psi,Bilin(5,9,9) *psi)
+Bilin(1,5,16)
 
 DM = Sparse_DM_Build(normalize(Collapse(1,5,9,psi)))
 Partial_Trace(DM,[1,2,3,4,6,7,8,9],9)
-
 
 
 
